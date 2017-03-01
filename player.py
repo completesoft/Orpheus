3ac1bcd10f1501f51561d53fdaa884d5598d1a06
docent2010 <docent2010@ukr.net>
2017-03-01 21:49:22 +0200

    Юбилейный коммит

--- player.py
+++ player.py
@@ -1,83 +1,93 @@
 import vlc
 import time
 import datetime
+import time
+import requests
+import json
+
 
 ALIVE_DELAY_SEC = 1
 NETWORK_CACHE_MS = 3000
 VERBOSE_LEVEL = 0
-
-DEFAULT_URL = 'http://ic3.101.ru:8000/v3_1'
-
-DEBUG_REKLAMA_1 = "19:23:00"
-DEBUG_REKLAMA_2 = "20:27:30"
+TIME_FORMAT = "%H:%M:%S"
+URL = "http://hive.product.in.ua:8885/api"
+PERIOD_UPDATE = 60
 
 
-def send_alive():
-    print("Send hive alive request")
+def req_schedule():
+    headers = {"content-type": "application/json"}
+    data = {"action": "get_schedule", "id": 1}
+    p = requests.post(URL, headers=headers, json=data)
+    return p.json()
 
+
+def current_time():
+    return datetime.datetime.strftime(datetime.datetime.now(), TIME_FORMAT)
+
+
+def delta_time(time_insert, minutes=1):
+    p = datetime.datetime.strftime(datetime.datetime.strptime(time_insert, TIME_FORMAT) + datetime.timedelta(minutes=minutes), TIME_FORMAT)
+    return p
+
+
+schedule = req_schedule()
+
+# DEBUG
+# json.dump(schedule, open("schedule.json", "w"), indent=4, sort_keys=True)
 
 i = vlc.Instance('--verbose={0} --network-caching={1}'.format(VERBOSE_LEVEL, NETWORK_CACHE_MS).split())
-p = i.media_player_new()    # player for main stream
-p.set_mrl(DEFAULT_URL)      # set main stream url
 
-p_r = i.media_player_new()  # player for reklama
+# player for main stream
+p_main = i.media_player_new()
 
+# set main stream url
+p_main.set_mrl(schedule["main_stream"]["url"])
+p_main.audio_set_volume(schedule["main_stream"]["volume"])
 
-schedule = [{"desc": "r_1", "time": "19:22:00", "state": "ready", "url": 'http://hive.product.in.ua/music/reklama_1.mp3'},
-            {"desc": "r_2", "time": "19:24:00", "state": "ready", "url": 'http://hive.product.in.ua/music/reklama_2.mp3'}]
+# player for advertising
+p_r = i.media_player_new()
 
-p.play()    # start main stream
+p_main.play()
 
-# debug_time = datetime.datetime.strftime(datetime.datetime.now() + datetime.timedelta(seconds=5), "%H:%M:%S")
-# schedule[0]["time"] = debug_time
-
-# DEBUG!!!
-schedule[0]["time"] = DEBUG_REKLAMA_1
-schedule[1]["time"] = DEBUG_REKLAMA_2
+count_start = current_time()
 
 while True:
 
-    print(schedule)
-    current_time = datetime.datetime.strftime(datetime.datetime.now(), "%H:%M:%S")
+    # if datetime.datetime.strptime(schedule["silent"], TIME_FORMAT)
+    count_end = current_time()
+    if datetime.timedelta(minutes = count_end-count_start) > PERIOD_UPDATE:
+        p_main.stop()
+        schedule = req_schedule()
+        p_main.audio_set_volume(schedule["main_stream"]["volume"])
+        p_main.play()
 
-    print(current_time)
-
-    for task in schedule:
+    for adv_sing in schedule["inserts"]:
         # delta time = task time + 1 minute
-        delta_time = datetime.datetime.strftime(datetime.datetime.strptime(task["time"], "%H:%M:%S") + datetime.timedelta(minutes=1), "%H:%M:%S")
-        if task["time"] <= current_time <= delta_time:  # if time in interval then run task
-            if task["state"] == "ready":
-                print("start {}".format(task["desc"]))
+        cur_time = current_time()
+        delta_t = delta_time(adv_sing["time"])
 
-                p_r.set_mrl(task["url"])    # set url for reklama
+        # if time in interval then run task
+        if adv_sing["time"] <= cur_time <= delta_t and "state" not in adv_sing.keys():
 
-                for x in range(1, 5):
-                    print("volume down")
-                    p.audio_set_volume((10-x)*10)
-                    time.sleep(1)
+            # set url for reklama
+            p_r.set_mrl(adv_sing["url"])
 
-                p_r.play()  # start reklama
+            for x in range(1, 6):
+                print("volume down")
+                p_main.audio_set_volume((10-x)*10)
                 time.sleep(1)
-                print("reklama start!!!")
 
-            if task["state"] == "ready" and p_r.is_playing():
-                task["state"] = "playing"
-                print("state play")
+            # start reklama
+            p_r.play()
+            time.sleep(1)
 
-            if task["state"] == "playing" and not p_r.is_playing():
-                    task["state"] = "old"
-                    # p.play()
-                    for x in range(5, 11):
-                        print("volume up")
-                        p.audio_set_volume(x * 10)
-                        time.sleep(1)
+            while p_r.is_playing():
+                time.sleep(1)
 
-    # is_playing = vlc.libvlc_media_player_is_playing(p)
-    # print(is_playing)
-    # if not is_playing:
-    #     p.stop()
-    #     print("restart")
-    #     p.play()
-    time.sleep(1)
-    # # send_alive()
+            adv_sing["state"]= False
+
+            for x in range(5, 11):
+                print("volume up")
+                p_main.audio_set_volume(x*10)
+                time.sleep(1)
 
