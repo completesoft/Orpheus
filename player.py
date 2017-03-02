6358ee5fd05fd83c76c6cd287af178424b97715d
simersim <simer@f1.mk.ua>
2017-03-02 20:34:45 +0200

    Версия 2017030203
    Добавленые плавные переходы громкости в тишину и обратно.
    Добавлена информация для отладки.
    Добавлена обработка ошибок подключения к серверу API.
    Исправлены ошибки.

--- player.py
+++ player.py
@@ -2,17 +2,19 @@ import vlc
 import datetime
 import time
 import requests
+import copy
 
 
 ALIVE_DELAY_SEC = 1
 NETWORK_CACHE_MS = 3000
 VERBOSE_LEVEL = 0
+DEBUG = True
 TIME_FORMAT = "%H:%M:%S"
 URL = "http://hive.product.in.ua:8885/api"
 # URL = "http://localhost:8885/api"
 
 # Quantity of loops main "While"-cycle
-PERIOD_UPDATE = 30
+PERIOD_UPDATE = 10
 
 
 def req_schedule():
@@ -30,26 +32,41 @@ def delta_time(time_insert, minutes=1):
     p = datetime.datetime.strftime(datetime.datetime.strptime(time_insert, TIME_FORMAT) + datetime.timedelta(minutes=minutes), TIME_FORMAT)
     return p
 
+
+def fade_out(player):
+    for x in range(1, 6):
+        print("volume down")
+        player.audio_set_volume((10 - x) * 10)
+        time.sleep(1)
+
+
+def fade_in(player):
+    for x in range(6, 11):
+        print("volume up")
+        player.audio_set_volume(x * 10)
+        time.sleep(1)
+
+
 # Receive schedule at the beginning
 schedule = req_schedule()
+inserts_array = copy.deepcopy(schedule["inserts"])
 
 i = vlc.Instance('--verbose={0} --network-caching={1}'.format(VERBOSE_LEVEL, NETWORK_CACHE_MS).split())
 
 # player for main stream
 p_main = i.media_player_new()
 
+# player for advertising
+p_r = i.media_player_new()
+
 # set main stream url
 p_main.set_mrl(schedule["main_stream"]["url"])
 p_main.audio_set_volume(schedule["main_stream"]["volume"])
-
-# player for advertising
-p_r = i.media_player_new()
 
 p_main.play()
 
 # Counter for update method
 counter_update = 0
-
 sleep_flag = False
 
 while True:
@@ -57,51 +74,76 @@ while True:
     time.sleep(1)
 
     counter_update += 1
+    if DEBUG:
+        print("COUNTER: "+str(counter_update))
 
     # Update schedule
     if counter_update > PERIOD_UPDATE:
         main_stream_old = schedule["main_stream"]["url"]
-        schedule = req_schedule()
-        if main_stream_old != schedule["main_stream"]["url"]:
-            p_main.stop()
-            p_main.set_mrl(schedule["main_stream"]["url"])
-            p_main.audio_set_volume(schedule["main_stream"]["volume"])
-        if not sleep_flag and not p_main.is_playing():
-            p_main.play()
+        try:
+            new_schedule = req_schedule()
+        except:
+            print("API timeout")
+        else:
+            if new_schedule != schedule:
+                if DEBUG:
+                    print("New schedule arrived")
+                    print("--------------------")
+                    print(schedule)
+                    print(new_schedule)
+                    print("--------------------")
+
+                schedule = new_schedule
+                inserts_array = copy.deepcopy(schedule["inserts"])
+
+            if DEBUG:
+                print(schedule)
+            if main_stream_old != schedule["main_stream"]["url"]:
+                p_main.stop()
+                p_main.set_mrl(schedule["main_stream"]["url"])
+                p_main.audio_set_volume(schedule["main_stream"]["volume"])
+            if not sleep_flag and not p_main.is_playing():
+                if DEBUG:
+                    print("restart stream")
+                p_main.stop()
+                p_main.play()
+
         counter_update = 0
 
-    ##### Check in "Silent"-zone
+    # ####Check in "Silent"-zone
     for silent in schedule["silent"]:
         if silent["time_start"] <= current_time() <= silent["time_end"]:
-            p_main.stop()
-            p_r.stop()
-            sleep_flag = True
+            if not sleep_flag:
+                fade_out(p_main)
+                p_main.stop()
+                sleep_flag = True
             break
         else:
-            sleep_flag = False
-
+            if sleep_flag:
+                p_main.play()
+                fade_in(p_main)
+                sleep_flag = False
 
     if sleep_flag:
         continue
     #####
 
     # Check: are we in INSERTS
-    for inserts in schedule["inserts"]:
+
+    for insert in inserts_array:
         cur_time = current_time()
-        delta_t = delta_time(inserts["time"])
-
+        delta_t = delta_time(insert["time"])
+        if DEBUG:
+            print(insert)
         # if time in interval then run task
-        if inserts["time"] <= cur_time <= delta_t and "state" not in inserts.keys():
-
+        if insert["time"] <= cur_time <= delta_t and "played" not in insert:
             # set url for reklama
-            p_r.set_mrl(inserts["url"])
+            p_r.set_mrl(insert["url"])
 
-            for x in range(1, 6):
-                print("volume down")
-                p_main.audio_set_volume((10 - x) * 10)
-                time.sleep(1)
+            fade_out(p_main)
 
             # start reklama
+            p_r.audio_set_volume(insert["volume"])
             p_r.play()
             time.sleep(1)
 
@@ -109,12 +151,9 @@ while True:
                 time.sleep(1)
 
             # Flag: the insert's played
-            inserts["state"] = False
+            insert["played"] = True
 
-            for x in range(5, 10):
-                print("volume up")
-                p_main.audio_set_volume(x * 10)
-                time.sleep(1)
+            fade_in(p_main)
 
     if not p_main.is_playing():
         p_main.stop()
