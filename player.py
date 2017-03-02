8e9b970a2e07c62ebed345e48b7a793ba0e1dfb6
docent2010 <docent2010@ukr.net>
2017-03-02 12:57:07 +0200

    2017030201
    First production

--- player.py
+++ player.py
@@ -1,17 +1,18 @@
 import vlc
-import time
 import datetime
 import time
 import requests
-import json
 
 
 ALIVE_DELAY_SEC = 1
 NETWORK_CACHE_MS = 3000
 VERBOSE_LEVEL = 0
 TIME_FORMAT = "%H:%M:%S"
-URL = "http://hive.product.in.ua:8885/api"
-PERIOD_UPDATE = 60
+# URL = "http://hive.product.in.ua:8885/api"
+URL = "http://localhost:8885/api"
+
+# Quantity of loops main "While"-cycle
+PERIOD_UPDATE = 30
 
 
 def req_schedule():
@@ -29,11 +30,8 @@ def delta_time(time_insert, minutes=1):
     p = datetime.datetime.strftime(datetime.datetime.strptime(time_insert, TIME_FORMAT) + datetime.timedelta(minutes=minutes), TIME_FORMAT)
     return p
 
-
+# Receive schedule at the beginning
 schedule = req_schedule()
-
-# DEBUG
-# json.dump(schedule, open("schedule.json", "w"), indent=4, sort_keys=True)
 
 i = vlc.Instance('--verbose={0} --network-caching={1}'.format(VERBOSE_LEVEL, NETWORK_CACHE_MS).split())
 
@@ -49,32 +47,58 @@ p_r = i.media_player_new()
 
 p_main.play()
 
-count_start = current_time()
+# Counter for update method
+counter_update = 0
+
+sleep_flag = False
 
 while True:
+    # for something
+    time.sleep(1)
 
-    # if datetime.datetime.strptime(schedule["silent"], TIME_FORMAT)
-    count_end = current_time()
-    if datetime.timedelta(minutes = count_end-count_start) > PERIOD_UPDATE:
-        p_main.stop()
+    counter_update += 1
+
+    # Update schedule
+    if counter_update > PERIOD_UPDATE:
+        main_stream_old = schedule["main_stream"]["url"]
         schedule = req_schedule()
-        p_main.audio_set_volume(schedule["main_stream"]["volume"])
-        p_main.play()
+        if main_stream_old != schedule["main_stream"]["url"]:
+            p_main.stop()
+            p_main.set_mrl(schedule["main_stream"]["url"])
+            p_main.audio_set_volume(schedule["main_stream"]["volume"])
+        if not sleep_flag and not p_main.is_playing():
+            p_main.play()
+        counter_update = 0
 
-    for adv_sing in schedule["inserts"]:
-        # delta time = task time + 1 minute
+    ##### Check in "Silent"-zone
+    for silent in schedule["silent"]:
+        if silent["time_start"] <= current_time() <= silent["time_end"]:
+            p_main.stop()
+            p_r.stop()
+            sleep_flag = True
+            break
+        else:
+            sleep_flag = False
+
+
+    if sleep_flag:
+        continue
+    #####
+
+    # Check: are we in INSERTS
+    for inserts in schedule["inserts"]:
         cur_time = current_time()
-        delta_t = delta_time(adv_sing["time"])
+        delta_t = delta_time(inserts["time"])
 
         # if time in interval then run task
-        if adv_sing["time"] <= cur_time <= delta_t and "state" not in adv_sing.keys():
+        if inserts["time"] <= cur_time <= delta_t and "state" not in inserts.keys():
 
             # set url for reklama
-            p_r.set_mrl(adv_sing["url"])
+            p_r.set_mrl(inserts["url"])
 
             for x in range(1, 6):
                 print("volume down")
-                p_main.audio_set_volume((10-x)*10)
+                p_main.audio_set_volume((10 - x) * 10)
                 time.sleep(1)
 
             # start reklama
@@ -84,10 +108,14 @@ while True:
             while p_r.is_playing():
                 time.sleep(1)
 
-            adv_sing["state"]= False
+            # Flag: the insert's played
+            inserts["state"] = False
 
-            for x in range(5, 11):
+            for x in range(5, 10):
                 print("volume up")
-                p_main.audio_set_volume(x*10)
+                p_main.audio_set_volume(x * 10)
                 time.sleep(1)
 
+    if not p_main.is_playing():
+        p_main.stop()
+        p_main.play()
